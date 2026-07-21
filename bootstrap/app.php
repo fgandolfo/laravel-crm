70d4921826a684764b3190a0a5ce0ebb6f994c65
fgandolfo <gandolfo.f@protonmail.com>
2026-07-21 18:54:42 +0200

    fixed dockerfile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,6 +29,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             EnsureFrontendRequestsAreStateful::class,
         ]);
+	
+	$middleware->trustProxies(at: '*')
+	
+	$middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
