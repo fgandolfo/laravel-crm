befb2524f5196272ee21b1aa88f514d70def36e7
Facundo Gandolfo <gandolfo.f@protonmail.com>
2026-07-21 17:44:45 +0200

    Add entrypoint script for PHP-FPM configuration

---
+++ docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+sed -i "s/listen = 9000/listen = 127.0.0.1:9000/" /usr/local/etc/php-fpm.d/www.conf || true
+php artisan config:cache || true
+php artisan route:cache || true
+php artisan storage:link || true
+exec supervisord -c /etc/supervisord.conf
